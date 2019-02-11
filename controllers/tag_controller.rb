36e6e5f172c5b0b940961fe9fb6aad2f45fd33ca
Gabor Budai <budaigabor1428@gmail.com>
2019-02-11 15:01:49 +0000

    tags work

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -3,12 +3,12 @@ require('sinatra/contrib/all')
 require_relative('../models/tag.rb')
 also_reload('../models/*')
 
-get 'money-tracker/tags' do
+get '/money-tracker/tags' do
   @tags = Tag.all()
   erb (:"tags/index")
 end
 
-get 'money-tracker/tags/:id' do
+get '/money-tracker/tags/:id' do
   @tags = Tag.find(params['id'].to_i)
   erb(:"tags/show")
 end
